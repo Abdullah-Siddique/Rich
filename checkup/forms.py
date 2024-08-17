1f6eba559371f78f0bb19bc102726961c2392e97
Abdullah Siddique <siddiqueabdullah581@gmail.com>
2024-08-17 23:40:22 +0600

    My app

--- checkup/forms.py
+++ checkup/forms.py
@@ -6,3 +6,20 @@ class UserProfileForm(forms.ModelForm):
         model = UserProfile
         fields = ['height', 'weight', 'age', 'gender']
 
+    def clean_height(self):
+        height = self.cleaned_data.get('height')
+        if height <= 0:
+            raise forms.ValidationError("Height must be greater than 0.")
+        return height
+
+    def clean_weight(self):
+        weight = self.cleaned_data.get('weight')
+        if weight <= 0:
+            raise forms.ValidationError("Weight must be greater than 0.")
+        return weight
+
+    def clean_age(self):
+        age = self.cleaned_data.get('age')
+        if age <= 0:
+            raise forms.ValidationError("Age must be greater than 0.")
+        return age
